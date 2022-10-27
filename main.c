f04d7314e4a6f6d1e4e430cb8a40e4f21b0492ea
Arata <arata04962@gmail.com>
2022-10-27 20:42:31 +0900

    feat: pass arguments to command

--- main.c
+++ main.c
@@ -5,14 +5,30 @@
 
 typedef struct {
   char *file;
+  char **args;
+  size_t args_len;
 } command_t;
 
 command_t *parse_command(char *command_str) {
   int command_len = strlen(command_str);
-  command_str[command_len - 1] = '\0';
+  command_str[command_len - 1] = ' ';
+
+  char **args = malloc(sizeof(char *) * 256);
+  char *file = strtok(command_str, " ");
+
+  int i = 0;
+  char *p = file;
+  while (p) {
+    p = strtok(NULL, " ");
+    args[i] = p;
+    i++;
+  }
 
   command_t *command = malloc(sizeof(command_t));
   command->file = command_str;
+  command->args = args;
+  command->args_len = i;
+
   return command;
 }
 
@@ -23,8 +39,15 @@ void exec_command(command_t *command) {
   }
 
   if (pid == 0) {
-    char *command_args[] = {command->file, NULL};
-    if (execvp(command->file, command_args) == -1) {
+    size_t argv_len = command->args_len + 2;
+    char **argv = malloc(sizeof(char *) * argv_len);
+    argv[0] = command->file;
+    argv[argv_len - 1] = NULL;
+    for (size_t i = 0; i < command->args_len; i++) {
+      argv[i + 1] = command->args[i];
+    }
+
+    if (execvp(command->file, argv) == -1) {
       perror("execvp");
       exit(1);
     }
@@ -44,6 +67,7 @@ int main() {
 
     command_t *command = parse_command(command_buf);
     exec_command(command);
+    free(command->args);
     free(command);
   }
 
