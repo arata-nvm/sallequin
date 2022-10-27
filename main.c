7af40150e2309ce77f846eedc25abdc7cd20129b
Arata <arata04962@gmail.com>
2022-10-27 20:15:11 +0900

    refactor: command struct

--- main.c
+++ main.c
@@ -3,9 +3,38 @@
 #include <string.h>
 #include <unistd.h>
 
+typedef struct {
+  char *file;
+} command_t;
+
+command_t *parse_command(char *command_str) {
+  int command_len = strlen(command_str);
+  command_str[command_len - 1] = '\0';
+
+  command_t *command = malloc(sizeof(command_t));
+  command->file = command_str;
+  return command;
+}
+
+void exec_command(command_t *command) {
+  pid_t pid = fork();
+  if (pid == -1) {
+    perror("fork");
+  }
+
+  if (pid == 0) {
+    char *command_args[] = {command->file, NULL};
+    if (execvp(command->file, command_args) == -1) {
+      perror("execvp");
+      exit(1);
+    }
+  } else {
+    waitpid(pid, NULL, 0);
+  }
+}
+
 int main() {
   char command_buf[256];
-  int command_len;
 
   for (;;) {
     printf("$ ");
@@ -13,23 +42,9 @@ int main() {
       exit(0);
     }
 
-    command_len = strlen(command_buf);
-    command_buf[command_len - 1] = '\0';
-
-    pid_t pid = fork();
-    if (pid == -1) {
-      perror("fork");
-    }
-
-    if (pid == 0) {
-      char *command_args[] = {command_buf, NULL};
-      if (execvp(command_buf, command_args) == -1) {
-        perror("execvp");
-        exit(1);
-      }
-    } else {
-      waitpid(pid, NULL, 0);
-    }
+    command_t *command = parse_command(command_buf);
+    exec_command(command);
+    free(command);
   }
 
   return 0;
