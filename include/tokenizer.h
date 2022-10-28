522a2f8f2b1964db9827547c230bf86a1705857a
Arata <arata04962@gmail.com>
2022-10-28 11:54:44 +0900

    refactor: delete unused functions

--- include/tokenizer.h
+++ include/tokenizer.h
@@ -11,8 +11,6 @@ typedef struct {
   char *literal;
 } token_t;
 
-char *dump_tokentype_t(tokentype_t tokentype);
 
-void dump_token_t(token_t *token);
 
 char *next_token(char *s, token_t *out_token);
